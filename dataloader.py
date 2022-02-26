5d2bdd76c8231a8398fabcd6b704eaf16f99e76c
Chris <ucabilk@ucl.ac.uk>
2022-02-26 13:16:38 +0000

    asd

--- dataloader.py
+++ dataloader.py
@@ -1,2 +1,15 @@
 import pandas as pd
 import numpy as np
+import nltk
+from nltk.corpus import words as ws
+#Spacy?
+
+datafile = pd.read_excel("Data_8500_songs.xlsx")
+df = pd.DataFrame(datafile)
+
+df = df[['Artist', 'song', 'valence_tags', 'arousal_tags', 'dominance_tags', 'lyrics']]
+print(df.head())
+
+
+
+
