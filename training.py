7c37adb477bc839683d0e820c533f7f1e0f84f41
Chris <ucabilk@ucl.ac.uk>
2022-03-22 15:12:27 +0000

    converted training into a series of functions that can be called to allow for cross-va

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
+from sklearn.model_selection import cross_val_score
 import torch
 import torch.optim as optim
 import torch.nn as nn
@@ -45,122 +46,141 @@ def VA_to_quadrant(V, A):
 
 ##### Key Variables #####
 # Hashed values are those used in the reference paper
-EPOCHS = 40 #Until convergence
+EPOCHS = 3 #Until convergence
 BATCH_SIZE = 16 # 8
 LR = 3e-4 #2e-5
 USE_DOM = True
 FILENAME = '8500_songs_training.xlsx'
-ATTENTION_HEADS = 4 # 8
-EMBEDDING_SIZE = 64 # 512
+ATTENTION_HEADS = 8 # 8
+EMBEDDING_SIZE = 32 # 512
 NUM_ENCODER_LAYERS = 1 # 3
-FORWARD_XP = 32
+FORWARD_XP = 16
 DROPOUT = 0.1 # 0.1
 MAXLENGTH = 256 #1024
 MT_HEADS = 8 # 8
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using ', DEVICE)
 
-PRINT_STEP = 50
+PRINT_STEP = 100
 SAVE_STEP = 10  
 
 ##### Load Data into Dataset#####
-printc(f'Reading in {FILENAME} and creating dataloaders...')
-file = pd.read_excel(FILENAME)
-dataframe = pd.DataFrame(file)
-ps = PorterStemmer()
-stemmer = lambda x: ps.stem(x)
-dataset = dl.LSD_DataLoader(dataframe, 'lyrics', ['valence_tags', 'arousal_tags', 'dominance_tags'], MAXLENGTH)
-dataset.scale_VAD_scores(5, 5)
-dataset.clean_lyrics(remove_between_brackets=True, stem=True, stemmer=stemmer, tokenize=True, tokenizer=word_tokenize, length=MAXLENGTH)
-dataloader_tr = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+def generate_datasets(FILENAME):
+    printc(f'Reading in {FILENAME} and creating dataloaders...')
+    file = pd.read_excel(FILENAME)
+    dataframe = pd.DataFrame(file)
+    ps = PorterStemmer()
+    stemmer = lambda x: ps.stem(x)
+    dataset = dl.LSD_DataLoader(dataframe, 'lyrics', ['valence_tags', 'arousal_tags', 'dominance_tags'], MAXLENGTH)
+    dataset.scale_VAD_scores(5, 5)
+    dataset.clean_lyrics(remove_between_brackets=True, stem=True, stemmer=stemmer, tokenize=True, tokenizer=word_tokenize, length=MAXLENGTH)
+    dataloader_tr = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    return dataloader_tr, dataset, dataframe
 
 ##### Vocab work #####
-printc('Creating vocabulary from training data...')
-english = utils.Vocabulary(start_token='<SOS>', end_token='<EOS>', pad_token='<PAD>')
-english.creat_vocabulary(np.array(dataframe['lyrics']), max_size=30000, min_freq=5)
-PAD_IDX = english.pad_idx
-VOCAB_LEN = len(english)
-dataset.set_vocab(english)
-print(VOCAB_LEN)
-english.save('vocab_emb64_v2.pkl')
+def genereate_vocab(dataframe, dataset, save=False):
+    printc('Creating vocabulary from training data...')
+    english = utils.Vocabulary(start_token='<SOS>', end_token='<EOS>', pad_token='<PAD>')
+    english.creat_vocabulary(np.array(dataframe['lyrics']), max_size=30000, min_freq=5)
+    PAD_IDX = english.pad_idx
+    VOCAB_LEN = len(english)
+    dataset.set_vocab(english)
+    print(VOCAB_LEN)
+    if save:
+        english.save('vocab_emb64_v2.pkl')
+    return english, PAD_IDX, VOCAB_LEN
 
 ##### Prepare Model, Optimizer and Criterion #####
 print('Creating Models')
-multitask = mtn.multitaskNet(MT_HEADS, MAXLENGTH+2, EMBEDDING_SIZE, DROPOUT, DEVICE, 
-                            VOCAB_LEN, NUM_ENCODER_LAYERS, ATTENTION_HEADS, FORWARD_XP, 
-                            PAD_IDX, USE_DOM).to(DEVICE)
-multitask.double()
+def generate_models(VOCAB_LEN, PAD_IDX, EMBEDDING_SIZE, ATTENTION_HEADS, DROPOUT, USE_DOM, LR):
+    multitask = mtn.multitaskNet(MT_HEADS, MAXLENGTH+2, EMBEDDING_SIZE, DROPOUT, DEVICE, 
+                                VOCAB_LEN, NUM_ENCODER_LAYERS, ATTENTION_HEADS, FORWARD_XP, 
+                                PAD_IDX, USE_DOM).to(DEVICE)
+    multitask.double()
 
-adam = optim.AdamW(multitask.parameters(), lr=LR) # Fine tune this hypPs...
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(adam, factor=0.2, patience=10, verbose=True)
+    adam = optim.AdamW(multitask.parameters(), lr=LR) # Fine tune this hypPs...
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(adam, factor=0.2, patience=10, verbose=True)
 
-valence_L = nn.CrossEntropyLoss()
-arousal_L = nn.CrossEntropyLoss()
-dominance_L = nn.CrossEntropyLoss()
-quad_L = nn.CrossEntropyLoss()
+    valence_L = nn.CrossEntropyLoss()
+    arousal_L = nn.CrossEntropyLoss()
+    dominance_L = nn.CrossEntropyLoss()
+    quad_L = nn.CrossEntropyLoss()
+    return multitask, adam, scheduler, valence_L, arousal_L, dominance_L, quad_L
 
-losses = []
-valpoints = []
-aropoints = []
-true_quads = []
-multitask.train()
 # Training Loop
-print(f'Number of batches per epoch: {len(dataloader_tr)}')
-print(f'Printing every {PRINT_STEP} batches, saving every {SAVE_STEP} batches')
-for epoch in range(EPOCHS):
-    print(f'Epoch {epoch + 1} / {EPOCHS}')
-    epoch_losses = []
-    t = time.time()
-    for batch_idx, batch in enumerate(dataloader_tr):
-        inp_data = batch['lyrics'].to(DEVICE)
-        val = batch['valence_tags'].long().to(DEVICE)
-        aro = batch['arousal_tags'].long().to(DEVICE)
-        dom = batch['dominance_tags'].long().to(DEVICE)
-        quad = VA_to_quadrant(val, aro).to(DEVICE)
-        output, quad_pred = multitask(inp_data, version=0)
-        adam.zero_grad()
-        
-        valence_loss = valence_L(output[0], val)
-        arousal_loss = arousal_L(output[1], aro)
-        dominance_loss = dominance_L(output[2], dom) if USE_DOM == True else torch.tensor([0])
-        quad_loss = quad_L(quad_pred, quad)
-        loss = arousal_loss + valence_loss + dominance_loss + quad_loss
-        loss.backward()
-        #torch.nn.utils.clip_grad_norm_(multitask.parameters(), max_norm=1)
-        adam.step()
-        epoch_losses.append(loss.item())
-        if batch_idx != len(dataloader_tr) and (epoch + 1) % 8 == 0 : #ignore final batch since differnt size
-            valpoints.append(torch.squeeze(torch.softmax(output, dim=2)[0, :, 0]).detach().cpu().numpy())
-            aropoints.append(torch.squeeze(torch.softmax(output, dim=2)[1, :, 0]).detach().cpu().numpy())
+
+def train_model(EPOCHS, dataloader_tr, DEVICE, PRINT_STEP, SAVE_STEP, multitask, adam, scheduler, valence_L, arousal_L, dominance_L, quad_L):
+    print(f'Number of batches per epoch: {len(dataloader_tr)}')
+    print(f'Printing every {PRINT_STEP} batches, saving every {SAVE_STEP} batches')
+    losses = []
+    valpoints = []
+    aropoints = []
+    true_quads = []
+    multitask.train()
+    for epoch in range(EPOCHS):
+        print(f'Epoch {epoch + 1} / {EPOCHS}')
+        epoch_losses = []
+        t = time.time()
+        for batch_idx, batch in enumerate(dataloader_tr):
+            inp_data = batch['lyrics'].to(DEVICE)
+            val = batch['valence_tags'].long().to(DEVICE)
+            aro = batch['arousal_tags'].long().to(DEVICE)
+            dom = batch['dominance_tags'].long().to(DEVICE)
+            quad = VA_to_quadrant(val, aro).to(DEVICE)
+            output, quad_pred = multitask(inp_data, version=0)
+            adam.zero_grad()
+            
+            valence_loss = valence_L(output[0], val)
+            arousal_loss = arousal_L(output[1], aro)
+            dominance_loss = dominance_L(output[2], dom) if USE_DOM == True else torch.tensor([0])
+            quad_loss = quad_L(quad_pred, quad)
+            loss = arousal_loss + valence_loss + dominance_loss + quad_loss
+            loss.backward()
+            #torch.nn.utils.clip_grad_norm_(multitask.parameters(), max_norm=1)
+            adam.step()
+            epoch_losses.append(loss.item())
+            if batch_idx != len(dataloader_tr) and (epoch + 1) % 8 == 0 : #ignore final batch since differnt size
+                valpoints.append(torch.squeeze(torch.softmax(output, dim=2)[0, :, 0]).detach().cpu().numpy())
+                aropoints.append(torch.squeeze(torch.softmax(output, dim=2)[1, :, 0]).detach().cpu().numpy())
 
 
-        if (batch_idx + 1) % PRINT_STEP == 0:
-            print('')
-            #print(quad, val, aro)
-            #print(f'True Quadrant: {quad.detach().cpu().numpy()}, Predicted: {np.argmax(quad_pred.detach().cpu().numpy(), axis=1)}')
-            #print(f'Quadrant Scores: {quad_pred}')
-            print(f'Batch {batch_idx + 1} / {len(dataloader_tr)}')
-            #print('VAL', output[0, :2].detach().cpu().numpy(), val[:2].detach().cpu().numpy())
-            #print('ARO', output[1, :2].detach().cpu().numpy(), aro[:2].detach().cpu().numpy())
-            #print('DOM', output[2, :2].detach().cpu().numpy(), dom[:2].detach().cpu().numpy())
-            print(f'{PRINT_STEP} batch average loss:', np.average(epoch_losses[-PRINT_STEP:]))
-            scheduler.step(np.average(epoch_losses[-PRINT_STEP:]))
-        if (batch_idx + 1) % SAVE_STEP == 0:
-            losses.append(np.average(epoch_losses[-SAVE_STEP:]))
-    print(f'Epoch Time: {time.time() - t:.1f}s')
-    mean_loss = sum(epoch_losses) / len(epoch_losses)
+            if (batch_idx + 1) % PRINT_STEP == 0:
+                print('')
+                #print(quad, val, aro)
+                #print(f'True Quadrant: {quad.detach().cpu().numpy()}, Predicted: {np.argmax(quad_pred.detach().cpu().numpy(), axis=1)}')
+                #print(f'Quadrant Scores: {quad_pred}')
+                print(f'Batch {batch_idx + 1} / {len(dataloader_tr)}')
+                #print('VAL', output[0, :2].detach().cpu().numpy(), val[:2].detach().cpu().numpy())
+                #print('ARO', output[1, :2].detach().cpu().numpy(), aro[:2].detach().cpu().numpy())
+                #print('DOM', output[2, :2].detach().cpu().numpy(), dom[:2].detach().cpu().numpy())
+                print(f'{PRINT_STEP} batch average loss:', np.average(epoch_losses[-PRINT_STEP:]))
+                scheduler.step(np.average(epoch_losses[-PRINT_STEP:]))
+            if (batch_idx + 1) % SAVE_STEP == 0:
+                losses.append(np.average(epoch_losses[-SAVE_STEP:]))
+        print(f'Epoch Time: {time.time() - t:.1f}s')
+        #mean_loss = sum(epoch_losses) / len(epoch_losses)
 
-torch.save(multitask.state_dict(), 'MTL_clasification_50eps.pt')
+    torch.save(multitask.state_dict(), 'MTL_clasification_50eps.pt')
+    return losses
 
-#valpoints = np.concatenate(valpoints).reshape(-1)
-#aropoints = np.concatenate(aropoints).reshape(-1)
-#print(valpoints)
+dataloader_tr, dataset, dataframe = generate_datasets(FILENAME)
+english, PAD_IDX, VOCAB_LEN = genereate_vocab(dataframe, dataset, True)
+lrs = [5e-3, 1e-3, 5e-4, 1e-4]
+cross_val_losses = {l : [] for l in lrs}
+for lr in lrs:
+    print(f'Using LR {lr}')
+    multitask, adam, scheduler, valence_L, arousal_L, dominance_L, quad_L = generate_models(VOCAB_LEN, PAD_IDX, EMBEDDING_SIZE, ATTENTION_HEADS, DROPOUT, USE_DOM, lr)
+    cross_val_losses[lr] = train_model(2, dataloader_tr, DEVICE, PRINT_STEP, 5, multitask, adam, scheduler, valence_L, arousal_L, dominance_L, quad_L)
 
 print('Done')
-averaging_step = 10
-plt.plot(np.convolve(losses, np.ones(averaging_step) / averaging_step))
+#averaging_step = 10
+#plt.plot(np.convolve(losses, np.ones(averaging_step) / averaging_step))
 #plt.scatter(valpoints, aropoints, s=0.2, c=np.repeat(np.linspace(0, 1, int(len(valpoints) / BATCH_SIZE)), BATCH_SIZE))
 #plt.legend()
+fig, axs = plt.subplots(4)
+for i, e in enumerate(lrs):
+    axs[i].plot(cross_val_losses[e])
+    axs[i].set_title(f'LR: {lr}')
 plt.show()
 
 
