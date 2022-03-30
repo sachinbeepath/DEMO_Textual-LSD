a696572733803281df2cdde4f1814982fa105d05
BerokenScientist <berkemb23@hotmail.com>
2022-03-30 19:19:36 +0100

    training

--- training.py
+++ training.py
@@ -1,10 +1,9 @@
-from tkinter.tix import MAX
 import torch
 import utils
 
 ##### Key Variables #####
 # Hashed values are those used in the reference paper
-EPOCHS = 10 #Until convergence
+EPOCHS = 2 #Until convergence
 BATCH_SIZE = 64 # 8
 LR = 3e-3 #2e-5
 USE_DOM = True
@@ -19,19 +18,18 @@ MT_HEADS = 8 # 8
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using ', DEVICE)
 
-PRINT_STEP = 10
-SAVE_STEP = 5
+PRINT_STEP = 50
+SAVE_STEP = 10
 
-w2v = torch.load('w2v_weights.pkl')
+#w2v = torch.load('w2v_weights.pkl')
 
-trainer = utils.Textual_LSD_Training(verbose=True)
+trainer = utils.Textual_LSD_TVT(verbose=True)
 trainer.load_dataset(FILENAME, MAXLENGTH, BATCH_SIZE)
 trainer.load_vocab('vocab_emb64.pkl')
-trainer.generate_models(EMBEDDING_SIZE, ATTENTION_HEADS, DROPOUT, USE_DOM, 
-                        LR, MT_HEADS, NUM_ENCODER_LAYERS, FORWARD_XP, DEVICE, w2v=w2v, lr_pat=15)
+trainer.generate_models(EMBEDDING_SIZE, ATTENTION_HEADS, DROPOUT, USE_DOM,
+                        LR, MT_HEADS, NUM_ENCODER_LAYERS, FORWARD_XP, DEVICE, lr_pat=15)
 trainer.train(EPOCHS, PRINT_STEP, SAVE_STEP, 'Test.pt', enc_version=1)
-trainer.plot_data(averaging_window=5)
-
+trainer.plot_data(averaging_window=1)
 
 
 
