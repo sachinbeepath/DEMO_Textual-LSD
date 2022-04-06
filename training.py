c258f233f066bb7bb55064931371b065a1b464fb
Chris <ucabilk@ucl.ac.uk>
2022-04-06 12:49:49 +0100

    adwa

--- training.py
+++ training.py
@@ -4,18 +4,19 @@ import torch.nn.functional as F
 
 ##### Key Variables #####
 # Hashed values are those used in the reference paper
-EPOCHS = 1 #Until convergence
-BATCH_SIZE = 32 # 8
+EPOCHS = 4 #Until convergence
+BATCH_SIZE = 128 # 8
 LR = 3e-4 #2e-5
 USE_DOM = True
 FILENAME = 'Datasets/train_balanced.xlsx'
+VFILENAME = 'Datasets/validation_bal_3Apr.xlsx'
 ATTENTION_HEADS = 8 # 8
 EMBEDDING_SIZE = 32 # 512
 NUM_ENCODER_LAYERS = 1 # 3
-FORWARD_XP = 64
-DROPOUT = 0.25 # 0.1
-MAXLENGTH = 256 #1024
-MT_HEADS = 8 # 8
+FORWARD_XP = 3
+DROPOUT = 0.35 # 0.1
+MAXLENGTH = 128 #1024
+MT_HEADS = 64 # 8
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Using ', DEVICE)
 
@@ -27,12 +28,13 @@ print(w2v.shape)
 
 trainer = utils.Textual_LSD_TVT(verbose=True)
 trainer.load_dataset(FILENAME, MAXLENGTH, BATCH_SIZE)
+trainer.load_dataset(VFILENAME, MAXLENGTH, BATCH_SIZE, validation=True)
 #trainer.load_vocab('vocab_emb64.pkl')
-trainer.load_vocab('Pickles/Balanced_Vocab.pkl')
+trainer.load_vocab('Pickles/NewVocab.pkl')
 trainer.generate_models(EMBEDDING_SIZE, ATTENTION_HEADS, DROPOUT, USE_DOM,
                         LR, MT_HEADS, NUM_ENCODER_LAYERS, FORWARD_XP, DEVICE, lr_pat=15)
-trainer.train(EPOCHS, PRINT_STEP, SAVE_STEP, enc_version=1)
-trainer.plot_data(averaging_window=1)
+trainer.train(EPOCHS, PRINT_STEP, SAVE_STEP, enc_version=2, save_epochs=5, validation_freq=1)
+trainer.plot_data(averaging_window=5)
 
 
 
